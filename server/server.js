b44fa1f67ffe0bad13eeabebdc58f81b5fe53eaa
Austin Dahl <flyboy288@gmail.com>
2023-07-18 22:39:40 -0500

    Beginning of weather app project

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+googleApiKey = process.env.GOOGLE_API_KEY;
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log(`app is listening on port ${PORT}`);
+        console.log(`API key is ${googleApiKey}`);
+    }
+})
